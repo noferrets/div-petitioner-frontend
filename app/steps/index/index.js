32fe1d4b0c6b714c3bf2618daa0f25c6f22524f1
Tony Chow <tony.chow@hmcts.net>
2018-04-26 17:35:52 +0100

    Redirect straight from index

--- app/steps/index/index.js
+++ app/steps/index/index.js
@@ -1,31 +1,19 @@
 const Step = require('app/core/Step');
-const runStepHandler = require('app/core/handler/runStepHandler');
 const initSession = require('app/middleware/initSession');
-const applicationFeeMiddleware = require('app/middleware/updateApplicationFeeMiddleware');
 
 module.exports = class Index extends Step {
   get url() {
     return '/index';
   }
   get nextStep() {
-    return this.steps.ScreeningQuestionsMarriageBroken;
+    return this.steps.Start;
   }
 
   get middleware() {
-    return [
-      initSession,
-      applicationFeeMiddleware.updateApplicationFeeMiddleware
-    ];
+    return [ initSession ];
   }
 
   handler(req, res) {
-    return runStepHandler(this, req, res);
-  }
-
-  get ignorePa11yWarnings() {
-    return [
-      // Paragraph with 2 links in it but it's not semantically a list
-      'WCAG2AA.Principle1.Guideline1_3.1_3_1.H48'
-    ];
+    res.redirect(this.next().url);
   }
 };
--- app/steps/index/index.test.js
+++ app/steps/index/index.test.js
@@ -1,57 +1,32 @@
 const request = require('supertest');
-const { testContent, testRedirect } = require('test/util/assertions');
+const { testRedirect } = require('test/util/assertions');
 const server = require('app');
-const applicationFeeMiddleware = require('app/middleware/updateApplicationFeeMiddleware');
-const { expect, sinon } = require('test/util/chai');
 
 const modulePath = 'app/steps/index';
-const content = require(`${modulePath}/content`);
 
 const featureTogglesMock = require('test/mocks/featureToggles');
-const { withSession } = require('test/util/setup');
 
 let s = {};
 let agent = {};
 let underTest = {};
-const two = 2;
 
 describe(modulePath, () => {
   beforeEach(() => {
-    sinon.stub(applicationFeeMiddleware, 'updateApplicationFeeMiddleware')
-      .callsArgWith(two);
     featureTogglesMock.stub();
     s = server.init();
     agent = request.agent(s.app);
     underTest = s.steps.Index;
   });
 
-
   afterEach(() => {
     s.http.close();
     featureTogglesMock.restore();
-    applicationFeeMiddleware.updateApplicationFeeMiddleware.restore();
-  });
-
-  describe('#middleware', () => {
-    it('returns updateApplicationFeeMiddleware in middleware', () => {
-      expect(underTest.middleware
-        .includes(applicationFeeMiddleware.updateApplicationFeeMiddleware))
-        .to.eql(true);
-    });
   });
 
   describe('success', () => {
-    beforeEach(done => {
-      withSession(done, agent);
-    });
-
-    it('renders the content from the content file', done => {
-      testContent(done, agent, underTest, content);
-    });
-
-    it('redirects to the next page', done => {
+    it('redirects to the next page immediately', done => {
       testRedirect(done, agent, underTest, {},
-        s.steps.ScreeningQuestionsMarriageBroken);
+        s.steps.Start);
     });
   });
 });
